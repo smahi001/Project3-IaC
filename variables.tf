d2cb3ae0606af851491d45abc9b10b224f19efee
smahi001 <08monisha1996@gmail.com>
2025-07-18 16:07:29 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,8 @@
-variable "location" {
-  description = "Azure region"
-  default     = "southindia"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0, < 4.0"  # or ">= 3.0, < 5.0" to allow v4
+    }
+  }
 }
