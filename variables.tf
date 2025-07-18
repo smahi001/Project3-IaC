1a4d5b6cd2e772bd8c45c4facf6537ef1e786f4b
smahi001 <08monisha1996@gmail.com>
2025-07-18 17:53:34 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
 variable "location" {
   description = "Azure region"
+  type        = string
   default     = "southindia"
 }
-    
