dfd8fa0a58067bbe4627fa08ed41f15d372e46ab
smahi001 <08monisha1996@gmail.com>
2025-07-18 16:11:11 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = "~> 3.0"  # or your preferred version
+    }
+    random = {
+      source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
