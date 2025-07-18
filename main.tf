c54610ca4fe415652ff70a93851ebb5e1052b4c7
smahi001 <08monisha1996@gmail.com>
2025-07-18 18:35:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
 
 terraform {
@@ -9,4 +10,26 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = var.virtual_network_name
+  address_space       = var.address_space
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "subnets" {
+  count                = length(var.subnet_prefixes)
+  name                 = "subnet-${count.index}"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = [var.subnet_prefixes[count.index]]
 }
