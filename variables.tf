12a1f8e06ec37112722d5d4dc17f5635ecac24be
smahi001 <08monisha1996@gmail.com>
2025-07-18 19:38:13 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,12 @@
 variable "environment" {
-  description = "The deployment environment"
-  type        = string
-}
-
-variable "prefix" {
-  description = "Resource naming prefix"
+  description = "The deployment environment (dev, staging, production)"
   type        = string
 }
 
 variable "resource_group_name" {
-  description = "Azure Resource Group name"
+  description = "Name of the existing resource group"
   type        = string
+  default     = "project3-rg"
 }
 
 variable "location" {
@@ -19,46 +15,22 @@ variable "location" {
   default     = "southindia"
 }
 
-variable "virtual_network_name" {
+variable "vnet_name" {
   description = "Virtual Network name"
   type        = string
 }
 
-variable "address_space" {
+variable "vnet_address_space" {
   description = "Virtual Network address space"
   type        = list(string)
 }
 
-variable "subnet_prefixes" {
-  description = "Subnet address prefixes"
-  type        = list(string)
-}
-
-variable "vm_size" {
-  description = "Virtual Machine size"
-  type        = string
-  default     = "Standard_B1s"
-}
-
-variable "admin_username" {
-  description = "Admin username for VMs"
-  type        = string
-}
-
-variable "db_sku_name" {
-  description = "Database SKU name"
-  type        = string
-}
-
-variable "db_storage_mb" {
-  description = "Database storage in MB"
-  type        = number
-}
-
-variable "enable_monitoring" {
-  description = "Enable monitoring resources"
-  type        = bool
-  default     = false
+variable "subnets" {
+  description = "List of subnets"
+  type = list(object({
+    name           = string
+    address_prefix = string
+  }))
 }
 
 variable "tags" {
