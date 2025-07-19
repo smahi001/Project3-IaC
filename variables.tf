a0b2d6dc61c306bc6cf59bc3837ec37fdc12bbbb
smahi001 <08monisha1996@gmail.com>
2025-07-18 21:31:31 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
-variable "environment" {
-  description = "The deployment environment (dev, staging, production)"
-  type        = string
+variable "vnet_address_space" {
+  type        = list(string)
+  description = "Address space for the virtual network"
+  
+  validation {
+    condition     = can([for s in var.vnet_address_space : regex("^\\d+\\.\\d+\\.\\d+\\.\\d+/\\d+$", s)])
+    error_message = "Must be a valid CIDR block (e.g., 10.0.0.0/16)."
+  }
 }
 
 variable "resource_group_name" {
