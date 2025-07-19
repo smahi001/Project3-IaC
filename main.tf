eef25458c2a94d1a5078b4b533325db12ff57fb5
smahi001 <08monisha1996@gmail.com>
2025-07-18 20:21:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 terraform {
   backend "azurerm" {}
-  
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
 }
 
 provider "azurerm" {
@@ -14,7 +7,7 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-# Resources
+# Resource definitions here
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
