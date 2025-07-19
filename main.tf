bc72c572be31e0d8b5d1370d8d3e91abacbcee80
smahi001 <08monisha1996@gmail.com>
2025-07-18 20:11:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,54 +1,17 @@
 terraform {
-  backend "azurerm" {
-    # Backend config will be passed via CLI during init
+  backend "azurerm" {}
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
   skip_provider_registration = true
-}
-
-# Variables
-variable "environment" {
-  description = "The deployment environment (dev, staging, production)"
-  type        = string
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-  default     = "project3-rg"
-}
-
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "southindia"
-}
-
-variable "vnet_name" {
-  description = "Virtual Network name"
-  type        = string
-}
-
-variable "vnet_address_space" {
-  description = "Virtual Network address space"
-  type        = list(string)
-}
-
-variable "subnets" {
-  description = "List of subnets"
-  type = list(object({
-    name           = string
-    address_prefix = string
-  }))
-}
-
-variable "tags" {
-  description = "Resource tags"
-  type        = map(string)
-  default     = {}
 }
 
 # Resources
