79ae7cebd3e23e4bb3edd82171e90555998218ec
smahi001 <08monisha1996@gmail.com>
2025-07-18 21:39:41 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,45 +1,23 @@
-variable "vnet_address_space" {
-  type        = list(string)
-  description = "Address space for the virtual network"
-  
-  validation {
-    condition     = can([for s in var.vnet_address_space : regex("^\\d+\\.\\d+\\.\\d+\\.\\d+/\\d+$", s)])
-    error_message = "Must be a valid CIDR block (e.g., 10.0.0.0/16)."
-  }
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
+variable "environment" {
   type        = string
-  default     = "project3-rg"
-}
-
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "southindia"
-}
-
-variable "vnet_name" {
-  description = "Virtual Network name"
-  type        = string
-}
-
-variable "vnet_address_space" {
-  description = "Virtual Network address space"
-  type        = list(string)
+  description = "Deployment environment (dev/staging/prod)"
 }
 
 variable "subnets" {
-  description = "List of subnets"
   type = list(object({
     name           = string
     address_prefix = string
+    service_endpoints = optional(list(string))
+    nsg_rules = optional(list(object({
+      name                       = string
+      priority                   = number
+      direction                  = string
+      access                     = string
+      protocol                   = string
+      source_port_range          = string
+      destination_port_range     = string
+      source_address_prefix      = string
+      destination_address_prefix = string
+    })))
   }))
-}
-
-variable "tags" {
-  description = "Resource tags"
-  type        = map(string)
-  default     = {}
 }
